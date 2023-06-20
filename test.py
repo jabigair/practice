014b6820d354ee8f3d215e28da98fa6613c9ff8d
Abigail <abigailjones@pobox.com>
2023-06-20 10:43:11 -0400

    Added div
    Made sure to take care of division by zero case

--- test.py
+++ test.py
@@ -7,5 +7,11 @@ def subtract(n, m):
 def mul(a,b):
   return a * b
 
+def div(a, b):
+  if b == 0:
+      raise ValueError("Cannot divide by zero.")
+  else:
+      return a / b
+
 # some comments explaining the meaning of the universe
 # blah blah blue blah
